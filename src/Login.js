7c17420ec4d5684e31460630e8763ae163ec1bf1
J-Munny144 <94010822+J-Munny144@users.noreply.github.com>
2024-10-07 14:24:30 -0400

    Code for Auth0
    Separate branch because of bug

--- src/Login.js
+++ src/Login.js
@@ -1,51 +1,19 @@
-import React, {useState} from 'react';
+import React from 'react';
+import {useAuth0} from '@auth0/auth0-react';
 
-function Login({onLogin}) {
-    const [loginInfo, setLoginInfo] = useState({username: '', password: ''});
-
-    const handleInputChange = (e) => {
-        const { id, value } = e.target;
-        setLoginInfo({
-          ...loginInfo,
-          [id]: value
-        });
-      };
-    
-      const handleSubmit = (e) => {
-        e.preventDefault();
-    
-        // Hardcoded login for now
-        if (loginInfo.username === 'test' && loginInfo.password === 'tester123') {
-          onLogin(true);
-        } else {
-          alert('The username or password does not exist.');
-        }
-      };
-
-return (
-    <div>
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          id="username"
-          placeholder="Username"
-          value={loginInfo.username}
-          onChange={handleInputChange}
-          required
-        />
-        <input
-          type="password"
-          id="password"
-          placeholder="Password"
-          value={loginInfo.password}
-          onChange={handleInputChange}
-          required
-        />
-        <button type="submit">Login</button>
-      </form>
-    </div>
-  );
-}
+function Login() {
+    const { loginWithRedirect } = useAuth0();
+  
+    const handleLogin = () => {
+      loginWithRedirect();
+    };
+  
+    return (
+      <div>
+        <h2>Login</h2>
+        <button onClick={handleLogin}>Login with Auth0</button>
+      </div>
+    );
+  }
 
 export default Login;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {Auth0Provider} from '@auth0/auth0-react';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -7,11 +8,16 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-4b8ahp2zxggh7fuk.us.auth0.com"
+      clientId="6T8Zb26xsTW9M5LRFbvexkPNl5qLNx8o"
+      redirectUri={window.location.origin}
+      useRefreshTokens={true}
+      cacheLocation="localstorage"
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
